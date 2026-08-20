93826449a0dbeb80f72994303b92b982955f58f4
Kostya Kalitvyansky <kalitvyan@gmail.com>
2026-08-20 17:14:41 +0700

    ApplicationException

---
+++ src/Application/Exception/ApplicationException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Exception;
+
+use App\Shared\Exception\ApiException;
+
+abstract class ApplicationException extends ApiException
+{
+}
