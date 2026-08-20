1f8045ae27c0e2480f1093ff28a6a9276a6b2f42
Kostya Kalitvyansky <kalitvyan@gmail.com>
2026-08-20 12:26:56 +0700

    Update Db settings

--- config/db.php
+++ config/db.php
@@ -2,9 +2,14 @@
 
 return [
     'class' => \yii\db\Connection::class,
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => sprintf(
+        'pgsql:host=%s;port=%s;dbname=%s',
+        $_ENV['DB_HOST'],
+        $_ENV['DB_PORT'],
+        $_ENV['DB_NAME'],
+    ),
+    'username' => $_ENV['DB_USER'],
+    'password' => $_ENV['DB_PASSWORD'],
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
